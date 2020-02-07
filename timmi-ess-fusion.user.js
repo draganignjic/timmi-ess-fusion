36eef3fbd22021639dfd54d68cd116dd3aed036a
draganignjic <60779571+draganignjic@users.noreply.github.com>
2020-02-07 12:59:16 +0100

    Update timmi-ess-fusion.user.js

--- timmi-ess-fusion.user.js
+++ timmi-ess-fusion.user.js
@@ -10,8 +10,8 @@
 // @grant        GM.setValue
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js
-// @downloadURL  https://github.com/draganignjic/timmi-ess-fusion/raw/master/timmi-ess-fusion.user.js
-// @updateURL    https://github.com/draganignjic/timmi-ess-fusion/raw/master/timmi-ess-fusion.user.js
+// @downloadURL  https://raw.githubusercontent.com/draganignjic/timmi-ess-fusion/master/timmi-ess-fusion.user.js
+// @updateURL    https://raw.githubusercontent.com/draganignjic/timmi-ess-fusion/master/timmi-ess-fusion.user.js
 // ==/UserScript==
 
 (async () => {
